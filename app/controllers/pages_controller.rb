c491374820d8ed5c07ba83e4674ca61049864ab1
Mirza <mirza044@gmail.com>
2018-03-08 15:27:24 +0100

    test
    test

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+#The pages controller contains all of the code for any page inside of /pages
+class PagesController < ApplicationController
+  
+  #back-end code for pages/home
+  def index
+  end
+
+  #back-end code for pages/home
+  def home
+  end
+
+  #back-end code for pages/home
+  def profile
+  end
+
+  #back-end code for pages/home
+  def explore
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+
+  #Define root page
+
+  root 'pages#index'
+
+  #Define Routes for Pages
+
+#get 'pages/index' (delete this line)
+  get '/home' => 'pages#home'  #override default 'pages/home' routes
+  get '/profile' => 'pages#profile'
+  get '/explore' => 'pages#explore'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
