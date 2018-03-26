28927f64ace8a8728c37563d070cbfe73b085714
Mirza <mirza044@gmail.com>
2018-03-26 16:21:35 +0200

    zdravlje_01

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -38,4 +38,22 @@ class PagesController < ApplicationController
     @newPost = Post.new
     @toFollow=User.all.last(5)
   end
+
+  #back-end code for pages/home
+  
+  def index
+
+      #grab the username from the URL as :id
+    if (User.find_by_username(params[:id]))
+      @username = params[:id]
+    else
+      #redirect to 404 (root for now)
+      redirect_to root_path, :notice=> "User not found"
+    end
+
+    @posts = Post.all
+    @newPost = Post.new
+    @toFollow=User.all.last(5)
+  end
+
 end
