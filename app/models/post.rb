01bca80916d55430b25984769f452de649addc43
Mirza <mirza044@gmail.com>
2018-05-03 16:13:56 +0200

    comments for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 } #tweets are capped at 140 chars.
+  #validates :content, presence: true, length: { maximum: 140 } #tweets are capped at 140 chars.
+  validates :content, presence: true
   default_scope -> { order(created_at: :desc) } #newest tweets / posts first
 end
